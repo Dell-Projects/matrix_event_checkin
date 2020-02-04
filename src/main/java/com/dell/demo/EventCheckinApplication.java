32023033840dd94815af8fb4f6704678e34b3b39
Luis_Rebelo <Luis_rebelo@Dellteam.com>
2020-02-04 09:00:20 -0300

    Initial commit

---
+++ src/main/java/com/dell/demo/EventCheckinApplication.java
@@ -0,0 +1,13 @@
+package com.dell.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventCheckinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventCheckinApplication.class, args);
+	}
+
+}
