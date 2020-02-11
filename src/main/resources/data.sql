352891f278d4ef71533a0a12907894fc33aa1a70
Luis_Rebelo <Luis_rebelo@Dellteam.com>
2020-02-11 16:44:33 -0300

    More employees and events on the db mock

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -9,13 +9,23 @@ INSERT INTO employees (badge, name, role)
     VALUES (3, 'Daniel', 'Intern');
 
 INSERT INTO employees (badge, name, role)
-    VALUES (4, 'Adilson', 'Intern');
+    VALUES (4, 'Adilson', 'Badge');
 
 INSERT INTO employees (badge, name, role)
     VALUES (5, 'Nelson', 'Badge');
 
 INSERT INTO employees (badge, name, role)
     VALUES (6, 'Nicolas', 'Badge');
+
+INSERT INTO employees (badge, name, role)
+    VALUES (7, 'Jean', 'Contractor');
+
+INSERT INTO employees (badge, name, role)
+    VALUES (8, 'Wagner', 'Contractor');
+
+INSERT INTO employees (badge, name, role)
+    VALUES (9, 'Arthur', 'Contractor');
+
 
 --Events
 INSERT INTO EVENTS (id,
@@ -28,9 +38,9 @@ INSERT INTO EVENTS (id,
                     final_date,
                     has_gift)
      VALUES (seq_event.nextval,
-             'Dell Talk',
+             'Dell Talk - Docker',
              'Talk about docker and kubernetes',
-             3,
+             10,
              'Ping Pong table',
              'Open',
              TO_DATE('2020/02/12 14:00:00', 'yyyy/mm/dd hh24:mi:ss'),
@@ -46,17 +56,55 @@ INSERT INTO EVENTS (id,
                     date,
                     final_date,
                     has_gift)
-    VALUES (seq_event.nextval,
-           'Dell Talk',
-           'Talk about quality assurance',
-           30,
-           'Ping Pong table',
-           'Open',
-           TO_DATE('2020/01/02 10:00:00', 'yyyy/mm/dd hh24:mi:ss'),
-           TO_DATE('2020/01/02 13:00:00', 'yyyy/mm/dd hh24:mi:ss'),
-           1);
+     VALUES (seq_event.nextval,
+             'Dell Talk - Angular',
+             'Angular framework tips',
+             10,
+             'Ping Pong table',
+             'Open',
+             TO_DATE('2020/02/12 14:00:00', 'yyyy/mm/dd hh24:mi:ss'),
+             TO_DATE('2020/02/12 16:00:00', 'yyyy/mm/dd hh24:mi:ss'),
+             0);
 
---Employee_events
+INSERT INTO EVENTS (id,
+                    name,
+                    description,
+                    capacity,
+                    location,
+                    event_target,
+                    date,
+                    final_date,
+                    has_gift)
+     VALUES (seq_event.nextval,
+             'Matrix Hackathon',
+             'Interns hackathon (Role lock)',
+             15,
+             'Erico Verissimo',
+             'Intern',
+             TO_DATE('2020/02/12 14:00:00', 'yyyy/mm/dd hh24:mi:ss'),
+             TO_DATE('2020/02/12 16:00:00', 'yyyy/mm/dd hh24:mi:ss'),
+             1);
+
+INSERT INTO EVENTS (id,
+                    name,
+                    description,
+                    capacity,
+                    location,
+                    event_target,
+                    date,
+                    final_date,
+                    has_gift)
+    VALUES (seq_event.nextval,
+           'Tech talk - Spring',
+           'Talk about quality assurance (Date lock)',
+           20,
+           'Erico Verissimo',
+           'Open',
+           TO_DATE('2019/01/02 10:00:00', 'yyyy/mm/dd hh24:mi:ss'),
+           TO_DATE('2019/01/02 13:00:00', 'yyyy/mm/dd hh24:mi:ss'),
+           0);
+
+--CheckIn
 -- INSERT INTO EMPLOYEE_EVENTS (id,
 --                              employee_badge,
 --                              event_id,
