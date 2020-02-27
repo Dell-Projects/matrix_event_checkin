077ea75c92537859f9fac11eae60fdbc07bc6cf9
Luis_Rebelo <Luis_rebelo@Dellteam.com>
2020-02-27 14:40:05 -0300

    SQL script optimized

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,28 +1,22 @@
 --Employees
 INSERT INTO employees (badge, name, role)
-    VALUES (1, 'Luiz', 'Intern');
-
-INSERT INTO employees (badge, name, role)
-    VALUES (2, 'Lipe', 'Intern');
-
-INSERT INTO employees (badge, name, role)
-    VALUES (3, 'Daniel', 'Intern');
-
-INSERT INTO employees (badge, name, role)
-    VALUES (4, 'Adilson', 'Badge');
-
-INSERT INTO employees (badge, name, role)
-    VALUES (5, 'Nelson', 'Badge');
-
-INSERT INTO employees (badge, name, role)
-    VALUES (6, 'Nicolas', 'Badge');
-
-INSERT INTO employees (badge, name, role)
-    VALUES (7, 'Gabriel', 'Contractor');
-
-INSERT INTO employees (badge, name, role)
-    VALUES (8, 'Bernard', 'Contractor');
-
+    VALUES
+    (1, 'Luiz', 'Intern'),
+    (2, 'Daniel', 'Intern'),
+    (3, 'Lipe', 'Intern'),
+    (4, 'Adilson', 'Intern'),
+    (5, 'Jean', 'Badge'),
+    (6, 'Arthur', 'Badge'),
+    (7, 'Wagner', 'Badge'),
+    (8, 'Richard', 'Contractor'),
+    (9, 'Gabriel', 'Contractor'),
+    (10, 'Felipe', 'Contractor'),
+    (11, 'Renata', 'Intern'),
+    (12, 'Cleyson', 'Intern'),
+    (13, 'Matheus', 'Intern'),
+    (14, 'Mangan', 'Badge'),
+    (15, 'Laura', 'Badge'),
+    (16, 'Nelson', 'Badge');
 
 
 --Events
@@ -35,72 +29,52 @@ INSERT INTO EVENTS (id,
                     date,
                     final_date,
                     has_gift)
-     VALUES (seq_event.nextval,
-             'Dell Talk - Docker',
-             'Talk about docker and kubernetes',
-             10,
-             'Ping Pong table',
-             'Open',
-             TO_DATE('2020/02/12 14:00:00', 'yyyy/mm/dd hh24:mi:ss'),
-             TO_DATE('2020/12/12 16:00:00', 'yyyy/mm/dd hh24:mi:ss'),
-             0);
+     VALUES
+     (
+        seq_event.nextval,
+        'Dell Talk - Docker',
+        'Talk about docker and kubernetes',
+        10,
+        'Ping Pong table',
+        'Open',
+        TO_DATE('2020/02/12 14:00:00', 'yyyy/mm/dd hh24:mi:ss'),
+        TO_DATE('2020/12/12 16:00:00', 'yyyy/mm/dd hh24:mi:ss'),
+        0
+     ),
+     (
+        seq_event.nextval,
+        'Dell Talk - Angular',
+        'Angular framework tips (Capacity lock)',
+        2,
+        'Ping Pong table',
+        'Open',
+        TO_DATE('2020/02/12 14:00:00', 'yyyy/mm/dd hh24:mi:ss'),
+        TO_DATE('2020/12/12 16:00:00', 'yyyy/mm/dd hh24:mi:ss'),
+        0
+     ),
+     (
+        seq_event.nextval,
+        'Matrix Hackathon',
+        'Interns hackathon (Role lock)',
+        15,
+        'Erico Verissimo',
+        'Intern',
+        TO_DATE('2020/02/12 14:00:00', 'yyyy/mm/dd hh24:mi:ss'),
+        TO_DATE('2020/12/12 16:00:00', 'yyyy/mm/dd hh24:mi:ss'),
+        1
+     ),
+     (
+        seq_event.nextval,
+        'Tech talk - Spring',
+        'Talk about quality assurance (Date lock)',
+        20,
+        'Erico Verissimo',
+        'Open',
+        TO_DATE('2019/01/02 10:00:00', 'yyyy/mm/dd hh24:mi:ss'),
+        TO_DATE('2019/01/02 13:00:00', 'yyyy/mm/dd hh24:mi:ss'),
+        0
+     );
 
-INSERT INTO EVENTS (id,
-                    name,
-                    description,
-                    capacity,
-                    location,
-                    event_target,
-                    date,
-                    final_date,
-                    has_gift)
-     VALUES (seq_event.nextval,
-             'Dell Talk - Angular',
-             'Angular framework tips (Capacity lock)',
-             2,
-             'Ping Pong table',
-             'Open',
-             TO_DATE('2020/02/12 14:00:00', 'yyyy/mm/dd hh24:mi:ss'),
-             TO_DATE('2020/12/12 16:00:00', 'yyyy/mm/dd hh24:mi:ss'),
-             0);
-
-INSERT INTO EVENTS (id,
-                    name,
-                    description,
-                    capacity,
-                    location,
-                    event_target,
-                    date,
-                    final_date,
-                    has_gift)
-     VALUES (seq_event.nextval,
-             'Matrix Hackathon',
-             'Interns hackathon (Role lock)',
-             15,
-             'Erico Verissimo',
-             'Intern',
-             TO_DATE('2020/02/12 14:00:00', 'yyyy/mm/dd hh24:mi:ss'),
-             TO_DATE('2020/12/12 16:00:00', 'yyyy/mm/dd hh24:mi:ss'),
-             1);
-
-INSERT INTO EVENTS (id,
-                    name,
-                    description,
-                    capacity,
-                    location,
-                    event_target,
-                    date,
-                    final_date,
-                    has_gift)
-    VALUES (seq_event.nextval,
-           'Tech talk - Spring',
-           'Talk about quality assurance (Date lock)',
-           20,
-           'Erico Verissimo',
-           'Open',
-           TO_DATE('2019/01/02 10:00:00', 'yyyy/mm/dd hh24:mi:ss'),
-           TO_DATE('2019/01/02 13:00:00', 'yyyy/mm/dd hh24:mi:ss'),
-           0);
 
 --CheckIn
 -- INSERT INTO EMPLOYEE_EVENTS (id,
